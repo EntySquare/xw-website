f5f46f6c317afc5932b93ce3a2b3bbc415408f07
2568256629 <2568256629@qq.com>
2023-06-13 15:35:32 +0800

    对接首页接口

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,25 +1,27 @@
 <script lang="ts"></script>
 <script setup lang="ts">
 // 导入组件
-import { onMounted } from 'vue'
+import { onMounted, ref } from 'vue'
 import homeBannerVue from './components/home-banner.vue'
 import HomeBody from './components/home-body.vue'
 import HomeFooter from './components/home-footer.vue'
 import Ranking from './components/home-ranking.vue'
 import { getHomelist } from '@/apis/home'
-let homeList = {
-  code: Number,
-  json: {
-    head_album: {}
+const bannerList = ref([])
+const hotList = ref([])
+const topList = ref([])
+const typeList = ref([])
+onMounted(async () => {
+  try {
+    const res = await getHomelist({ time_type: 0 });
+    bannerList.value = res.data?.json?.head_album || [];
+    hotList.value = res.data?.json?.hot || [];
+    topList.value = res.data?.json?.top || [];
+    typeList.value = res.data?.json?.type_list || [];
+    console.log(bannerList.value);
+  } catch (error) {
+    console.error(error);
   }
-}
-onMounted(() => {
-  getHomelist({
-    "time_type": 0
-  }).then(res => {
-    homeList = res.data || {};
-    console.log(homeList.json);
-  });
 })
 </script>
 
@@ -27,7 +29,7 @@ onMounted(() => {
   <div class="page-home">
     <div class="container">
       <!-- banner轮播图 -->
-      <homeBannerVue :homeDataList="homeList.json.head_album || {}" />
+      <homeBannerVue :bannerDataList="bannerList" />
       <!-- 排行列表 -->
       <Ranking />
       <!-- 主体部分 -->
