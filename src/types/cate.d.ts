0234d388ebf431fe33abf4b6591b5d553073afd0
2568256629 <2568256629@qq.com>
2023-06-14 20:39:26 +0800

    对接详情接口

--- src/types/cate.d.ts
+++ src/types/cate.d.ts
@@ -8,6 +8,15 @@ export type TBanner = {
   locking_time: number;
   opening_time: number;
 };
+export type BodyItemList = {
+  album_id: number;
+  album_img: string;
+  album_name: string;
+  author_name: string;
+  follow: Boolean;
+  pay_size: number;
+  region_price: string;
+};
 // 定义类型
 export type TRes<T> = {
   result: T;
--- src/views/rankings/components/ranking.vue
+++ src/views/rankings/components/ranking.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-import { onBeforeUnmount, ref } from 'vue'
+import { topAll } from '@/apis/ranking'
+import { onBeforeUnmount, onMounted, ref } from 'vue'
 
 const imgurl =
   'https://i.seadn.io/gcs/files/266274c8643e1c9d0659cb60bf06f740.png?auto=format&w=384'
@@ -13,6 +14,35 @@ getWindowInfo()
 onBeforeUnmount(() => {
   document.removeEventListener('resize', getWindowInfo)
 })
+const changeTimeType = ref('24小时')
+let timeType = 0
+let hotList = ref([])
+const getHotLlst = async () => {
+  try {
+    const res = await topAll({ time_type: timeType });
+    hotList.value = res.data?.json?.data_list || [];
+    console.log(hotList.value);
+  } catch (error) {
+    console.error(error);
+  }
+}
+getHotLlst();
+const handleSelect = (e) => {
+  if (e == 0) {
+    changeTimeType.value = '24小时'
+  }
+  if (e == 1) {
+    changeTimeType.value = '7天'
+  }
+  if (e == 2) {
+    changeTimeType.value = '30天'
+  }
+  timeType = e
+  getHotLlst()
+}
+onMounted(() => {
+
+})
 </script>
 <script lang="ts">
 export default {
@@ -25,7 +55,15 @@ export default {
       <template #extra>
         <div style="display: flex; gap: 10px">
           <a-button type="outline">查看全部</a-button>
-          <a-button type="primary">24小时 <icon-caret-down /> </a-button>
+          <!-- <a-button type="primary">24小时 <icon-caret-down /> </a-button> -->
+          <a-dropdown @select="handleSelect">
+            <a-button>{{ changeTimeType || '' }}<icon-down /></a-button>
+            <template #content>
+              <a-doption value="0">24小时</a-doption>
+              <a-doption value="1">7天</a-doption>
+              <a-doption value="2">30天</a-doption>
+            </template>
+          </a-dropdown>
         </div>
       </template>
       <a-tab-pane key="1">
@@ -55,51 +93,50 @@ export default {
                 <a-typography-title :heading="6"> 涨幅 </a-typography-title>
               </div>
             </div>
-            <div class="item" v-for="(item, index) in 3" :key="index">
+            <div class="item" v-for="(item, index) in hotList" :key="item.id">
               <div>
                 <a-typography-title type="secondary" :heading="6">
                   {{ index + 1 }}
                 </a-typography-title>
               </div>
               <div class="imgcar">
-                <LockDiv
-                  :width="!mqList ? '135px' : '73px'"
-                  :xyb="!mqList ? '135px' : '73px'"
-                >
+                <LockDiv :width="!mqList ? '135px' : '73px'" :xyb="!mqList ? '135px' : '73px'">
                   <a-avatar :size="!mqList ? 135 : 73" shape="square">
-                    <img :src="imgurl" />
+                    <img :src="item.image || ''" />
                   </a-avatar>
                 </LockDiv>
-                <a-typography-title :heading="5"> 醒目猴</a-typography-title>
+                <a-typography-title :heading="5"> {{ item.name || '' }}</a-typography-title>
                 <icon-check-circle-fill style="color: blue" :size="20" />
               </div>
               <div v-if="!mqList">
-                <a-typography-title :heading="6"> ¥500 </a-typography-title>
+                <a-typography-title :heading="6"> ¥{{ item.lowest_price || 0 }} </a-typography-title>
               </div>
               <div v-if="!mqList">
                 <a-typography-title :heading="6">
-                  ¥2342～¥5454
+                  ¥{{ item.lowest_price || 0 }}～¥{{ item.highest_price || 0 }}
                 </a-typography-title>
               </div>
               <div :style="{ flex: mqList ? 'none' : '5%' }">
-                <a-typography-title :heading="6"> 80000 </a-typography-title>
+                <a-typography-title :heading="6"> {{ item.pay_size || 0 }} </a-typography-title>
               </div>
               <div v-if="!mqList">
-                <a-typography-title :heading="6"> 100/50 </a-typography-title>
+                <a-typography-title :heading="6"> {{ item.nft_count || 0 }}/{{ item.nft_user_hold_count || 0 }}
+                </a-typography-title>
               </div>
               <div v-if="!mqList">
-                <a-typography-title :heading="6"> +7% </a-typography-title>
+                <a-typography-title :heading="6"> {{ item.float >= 0 ? '+' + item.float : item.float }}%
+                </a-typography-title>
               </div>
             </div>
           </div>
         </div>
       </a-tab-pane>
-      <a-tab-pane key="2">
+      <!-- <a-tab-pane key="2">
         <template #title>
           <p class="tap-p">热度榜</p>
         </template>
         <div class="tabsbody"></div>
-      </a-tab-pane>
+      </a-tab-pane> -->
     </a-tabs>
   </div>
 </template>
@@ -111,6 +148,7 @@ export default {
 
 .tabsbody {
   min-height: 100vh;
+
   .body-item {
     flex: 20%;
   }
@@ -130,7 +168,7 @@ export default {
       flex: 25%;
     }
 
-    > div {
+    >div {
       &:nth-child(1) {
         width: 8px;
         margin-right: 15px;
@@ -154,5 +192,9 @@ export default {
 
 :deep(.arco-tabs-nav-ink) {
   background-color: var(--color-neutral-10);
+}
+
+.arco-dropdown-open .arco-icon-down {
+  transform: rotate(180deg);
 }
 </style>
